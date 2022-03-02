9f5f81cf4b3cca51ff8a16351d88ab6cfe3f6480
12roshan12 <roshansharma7501@gmail.com>
2022-03-02 21:16:16 +0545

    changes

--- pharmaFront/src/app/app.module.ts
+++ pharmaFront/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { SharedComponent } from './shared/shared.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { SigninComponent } from './authentication/signin/signin.component';
+import { SignupComponent } from './authentication/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    SharedComponent,
+    DashBoardComponent,
+    OrdersComponent,
+    ProfilesComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
